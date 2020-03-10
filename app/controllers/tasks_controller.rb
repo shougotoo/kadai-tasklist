3355b2bb9d5533a41e0d208bb02b4060e4375beb
shougo.gotou <shogotoo0306@yahoo.co.jp>
2020-03-10 11:05:39 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,55 +1,46 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  include SessionsHelper
+
   def index
-    @tasks = Task.all
-  end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def create
-    @task = Task.new(task_params)
-    if @task.save
-      flash[:success] = 'Taskが正常に入力されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Taskが入力されませんでした'
-      render :new
-    end
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
-  def edit
-    @task = Task.find(params[:id])
+  def new
+    @task = current_user.tasks.build
   end
-
-  def update
-    @task = Task.find(params[:id])
-
-    if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
-      redirect_to @task
+  
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'タスクを入力しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Taskは更新されませんでした'
-      render :edit
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの入力に失敗しました。'
+      render 'toppages/index'
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
-    flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
-  private
-  
+   private
+
   def task_params
-    params.require(:task).permit(:content ,:status)
+    params.require(:task).permit(:content,:status)
   end
-
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
